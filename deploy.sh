23ae130a761e1244d7dd2818cf491f378082d4c3
Emiliano <47641194+Emilianoac@users.noreply.github.com>
2024-12-28 17:31:37 -0300

    Actualiza el script de despliegue y configura la base en vite.config.ts

--- deploy.sh
+++ deploy.sh
@@ -8,7 +8,7 @@ cd dist
 
 git init
 git add -A
-git commit -m 'Nuevo despliegue'
-git push -f git@github.com-pruebas:329439E932/ci-cd.git master:gh-pages
+git commit -m 'New Deployment'
+git push -f git@github-pruebas:329439E932/ci-cd.git master:gh-pages
 
 cd -
