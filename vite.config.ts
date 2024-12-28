ec73acd6a95021151c591dea092fbfd0c343287a
Emiliano <47641194+Emilianoac@users.noreply.github.com>
2024-12-28 17:38:11 -0300

    Actualiza la base en vite.config.ts para el nuevo entorno de CI/CD

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/emiliano-5-2024',
+  base: '/ci-cd',
   plugins: [
     vue(),
     vueDevTools(),
